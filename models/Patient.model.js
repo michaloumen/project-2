b9ba4c3414bbc021c40791a5a85c00f8d5983d96
mmanso <mayara.manso@mercadolivre.com>
2021-04-08 22:57:13 -0300

    creates model Patient

---
+++ models/Patient.model.js
@@ -0,0 +1,22 @@
+// User model here
+const { Schema, model, Types } = require("mongoose");
+
+const patientSchema = new Schema(
+  {
+    name: { type: String, required: true, minlength: 3, maxlength: 50 },
+    password: { type: String, required: true },
+    birthDate: { type: Date },
+    adress: { type: String },
+    email: { type: String, required: true },
+    phone: { type: String },
+    dentist: { type: [Types.ObjectId], ref: 'Dentist' },
+    returnDate: { type: Date },
+
+  },
+  {
+    timestamps: true,
+  }
+);
+const Patient = model('Patient', patientSchema);
+
+module.exports = Patient;
