cdde21afad734d4260f53ae037dd727ab96f6650
mmanso <mayara.manso@mercadolivre.com>
2021-04-10 12:34:07 -0300

    deletes some dependences

--- app.js
+++ app.js
@@ -1,42 +1,36 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const cookieParser = require("cookie-parser");
-const express = require("express");
-const favicon = require("serve-favicon");
-const hbs = require("hbs");
-const logger = require("morgan");
-const path = require("path");
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
 
 const app = express();
 
+const index = require('./routes/index.routes');
+const authPatientRoutes = require('./routes/authPatient.routes');
+const protectedRoutes = require('./routes/protectedRoutes.routes');
+
 // require database configuration
-require("./configs/db.config");
-require("./configs/session.config")(app);
+require('./configs/db.config');
+require('./configs/session.config')(app);
 
 // Middleware Setup
-app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 // Express View engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "hbs");
-hbs.registerPartials(path.join(__dirname, "views", "partials"));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 
-app.use(express.static(path.join(__dirname, "public")));
-/* app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico"))); */
+app.use(express.static(path.join(__dirname, 'public')));
+/* app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico'))); */
 
 // default value for title local
-app.locals.title = "Express - Generated with IronGenerator";
+app.locals.title = 'Express - Generated with IronGenerator';
 
-const index = require("./routes/index.routes");
-app.use("/", index);
-
-const authRoutes = require("./routes/authRoutes.routes");
-app.use("/", authRoutes);
-
-const protectedRoutes = require("./routes/protectedRoutes.routes");
-app.use("/", protectedRoutes);
+app.use('/', index);
+app.use('/', authPatientRoutes);
+app.use('/', protectedRoutes);
 
 module.exports = app;
