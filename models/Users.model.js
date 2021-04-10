3a9e72a73c9b101f45f775451b4e36300f578774
mmanso <mayara.manso@mercadolivre.com>
2021-04-10 14:30:15 -0300

    creat UserSchema and patientSchema and destistSchema

---
+++ models/Users.model.js
@@ -0,0 +1,34 @@
+const extendSchema = require('mongoose-extend-schema');
+const { Schema, model, Types } = require("mongoose");
+
+
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true, minlength: 3, maxlength: 50 },
+    password: { type: String, required: true },
+    birthDate: { type: Date },
+    adress: { type: String },
+    email: { type: String, required: true },
+    phone: { type: String },
+    role: { type: String, enum: ['patient', 'dentist'], default: 'patient' },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+const patientSchema = extendSchema(UserSchema, {
+  dentist: { type: [Types.ObjectId], ref: 'Dentist' },
+  returnDate: { type: Date },
+});
+
+const destistSchema = extendSchema(UserSchema, {
+  cro: { type: Number, required: true },
+  specialty: { type: [String] },
+});
+
+const Dentist = model('Dentist', destistSchema);
+const Patient = model('Patient', patientSchema);
+
+module.exports = { Patient, Dentist }
