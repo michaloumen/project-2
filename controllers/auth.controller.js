01b040cfb7e4ddd93355008f21422af1fc13fb53
mmanso <mayara.manso@mercadolivre.com>
2021-04-10 12:35:24 -0300

    creates AuthPatientController

---
+++ controllers/auth.controller.js
@@ -0,0 +1,45 @@
+const { isUserExists, isPasswordRigth } = require('../validations/auth.validations');
+const passwordManager = require('../utils/passwordManager');
+const Patient = require('../models/Patient.model');
+
+class AuthPatientController {
+
+  static async postCreateNewUser(req, res) {
+    try {
+      const { userEmail, userPassword } = req.body;
+
+      if (isUserExists(userEmail)) return res.render("auth-views/signup", { errorMessage: "Nome de usuário já existe. Por favor, escolha outro" });
+
+      const newUser = {
+        email: userEmail,
+        password: await passwordManager.encryptPassword(userPassword)
+      }
+
+      await Patient.create(newUser);
+
+      res.redirect('/login');
+    } catch (error) {
+      return next(error);
+    }
+  }
+
+  static async postLoginPatient(req, res) {
+    try {
+      const { userEmail, userPassword } = req.body;
+
+      if (!isUserExists(userEmail)) return res.render("auth-views/login", { errorMessage: 'Nome de usuário ou senha incorretos' });
+
+      const patientFromDB = await Patient.findOne({ email });
+
+      if (!isPasswordRigth(userPassword, userFromDB.password)) return res.render("auth-views/login", { errorMessage: 'Nome de usuário ou senha incorretos' });
+
+      req.session.currentUser = patientFromDB;
+
+      res.redirect("/main");
+    } catch (error) {
+      return "next(error)";
+    }
+  }
+}
+
+module.exports = AuthPatientController;
