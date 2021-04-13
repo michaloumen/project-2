a1e44ed7bb6d929459d6444710429b4a8666b77f
mmanso <mayara.manso@mercadolivre.com>
2021-04-12 21:31:39 -0300

    update auth-rotes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,31 +1,15 @@
 const router = require("express").Router();
-
-const isAuthDentist = require("../middlewares/verifyAuthDentist.middlewar");
 const AuthPatientController = require("../controllers/auth.controller");
 
 router.get("/signup", (req, res) => res.render("auth-views/signup"));
+router.post("/signup", async () => await AuthPatientController.postCreateNewUser);
 
-router.get("/dentist/signup", (req, res) =>
-  res.render("auth-views/dentistAuth")
-);
-router.post("/dentist/signup", async (req, res, next) => {
-  const { passwordSite } = req.body;
-  const isAuth = await isAuthDentist(passwordSite);
-  if (isAuth) {
-    return res.render("auth-views/signup");
-  }
-  return res.render("auth-views/dentistAuth", {
-    error: "Senha de acesso incorreta",
-  });
-});
+router.get("/dentist/signup", (req, res) => res.render("auth-views/dentistAuth"));
+router.post("/dentist/signup", async () => await AuthPatientController.authDentistRouteSignUp);
 
-router.post(
-  "/signup",
-  async () => await AuthPatientController.postCreateNewUser
-);
 
 router.get("/login", (req, res) => res.render("auth-views/login"));
-router.post("/login", async () => await AuthPatientController.postLoginPatient);
+router.post("/login", async () => await AuthPatientController.postLoginUser);
 
 router.get("/logout", (req, res) => {
   req.session.destroy();
