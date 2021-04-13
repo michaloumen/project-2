b1bba4db92af47cf2ee05b2bcc364af3abddcaa3
mmanso <mayara.manso@mercadolivre.com>
2021-04-12 21:33:07 -0300

    delete console.log in isAuthDentist

--- middlewares/verifyAuthDentist.middlewar.js
+++ middlewares/verifyAuthDentist.middlewar.js
@@ -1,14 +1,8 @@
 const isAuthDentist = (passwordSite) => {
-  console.log("michlle");
-
-  console.log("passwordSite", passwordSite);
-  console.log("process.env.DENTIST_SINGUP", process.env.DENTIST_SINGUP);
-
   if (passwordSite === process.env.DENTIST_SINGUP) {
     return true;
   }
 
-  console.log("não deu certo");
   return false;
 };
 
