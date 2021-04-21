c96623076038812607e096b29df2534a8b38b33a
mmanso <mayara.manso@mercadolivre.com>
2021-04-20 21:44:36 -0300

    add timeAvailable to schedule.type

--- models/types/schedule.types.js
+++ models/types/schedule.types.js
@@ -3,6 +3,7 @@ const { TIME, WEEKDAYS } = require('../../utils/contants.util');
 module.exports = {
   weekDay: { type: [Number], enum: WEEKDAYS },
   start: { type: String, enum: TIME },
-  end: { type: String, enum: TIME }
+  end: { type: String, enum: TIME },
+  timeAvailable: { type: [String], enum: TIME },
 };
 
