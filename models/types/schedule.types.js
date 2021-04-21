3d4f25e5095c74255cfc65832c6c1db3ca4546cb
mmanso <mayara.manso@mercadolivre.com>
2021-04-20 21:30:00 -0300

    create schedule.types

---
+++ models/types/schedule.types.js
@@ -0,0 +1,8 @@
+const { TIME, WEEKDAYS } = require('../../utils/contants.util');
+
+module.exports = {
+  weekDay: { type: [Number], enum: WEEKDAYS },
+  start: { type: String, enum: TIME },
+  end: { type: String, enum: TIME }
+};
+
