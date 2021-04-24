4a6c0cd6bcec2327bd6673f0474462d45fc9d514
mmanso <mayara.manso@mercadolivre.com>
2021-04-24 04:21:55 -0300

    fix lint scheduleAvailability

--- models/types/scheduleAvailability.types.js
+++ models/types/scheduleAvailability.types.js
@@ -1,9 +1,10 @@
-const { TIME, WEEKDAYS } = require('../../utils/contants.util');
+const { WEEK_DAYS, WORK_TIME } = require('../../utils/constants.util');
 
 module.exports = {
-  weekDay: { type: [Number], enum: WEEKDAYS },
-  start: { type: String, enum: TIME },
-  end: { type: String, enum: TIME },
-  timeAvailable: { type: [String], enum: TIME },
+  weekDay: { type: [Number], enum: WEEK_DAYS },
+  start: { type: String, enum: WORK_TIME },
+  end: { type: String, enum: WORK_TIME },
+  launchTimeStart: { type: String, enum: WORK_TIME },
+  launchTimeEnd: { type: String, enum: WORK_TIME },
 };
 
