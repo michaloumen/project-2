4860d774b1359869a97bd2dd1ee89e2ebf6368e2
mmanso <mayara.manso@mercadolivre.com>
2021-04-24 11:25:26 -0300

    add loggerUser

--- routes/dentist.routes.js
+++ routes/dentist.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 /* GET home page */
-router.get("/", (req, res, next) => res.render("dentist/home"));
+router.get("/", (req, res, next) => res.render("dentist/home", { loggedUser: req.session.currentUser }));
 router.get("/profile", (req, res, next) => res.render("dentist/profile"));
 
 module.exports = router;
