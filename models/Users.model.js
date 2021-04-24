14b259abb30d036694f780bce5d0a9890309cfdc
mmanso <mayara.manso@mercadolivre.com>
2021-04-24 04:19:33 -0300

    update UserSchema

--- models/Users.model.js
+++ models/Users.model.js
@@ -2,11 +2,11 @@ const extendSchema = require('mongoose-extend-schema');
 const { Schema, model, Types } = require("mongoose");
 
 const scheduleAvailability = require('./types/scheduleAvailability.types');
+const dateAvalable = require('./types/dateAvalable.type');
 
 const UserSchema = new Schema(
   {
     name: { type: String, required: true, minlength: 3, maxlength: 50 },
-    password: { type: String, required: true },
     birthDate: { type: Date },
     adress: { type: String },
     email: { type: String, required: true },
@@ -19,14 +19,17 @@ const UserSchema = new Schema(
 );
 
 const patientSchema = extendSchema(UserSchema, {
+  password: { type: String },
   dentist: { type: [Types.ObjectId], ref: 'Dentist' },
   returnDate: { type: Date },
 });
 
 const destistSchema = extendSchema(UserSchema, {
-  cro: { type: Number, required: true },
+  password: { type: String, required: true },
+  cro: { type: Number },
   specialty: { type: [String] },
   scheduleAvailability: { type: [Object], scheduleAvailability },
+  dateAvailable: { type: [Object], dateAvalable },
 });
 
 const Dentist = model('Dentist', destistSchema);
